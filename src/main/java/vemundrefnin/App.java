d5318df63b494e3a2bb314ef30c81c45fc6c8f73
Vemund Refnin <vemund.refnin@gmail.com>
2020-03-23 23:44:15 +0100

    test commands, die

--- src/main/java/vemundrefnin/App.java
+++ src/main/java/vemundrefnin/App.java
@@ -1,4 +1,7 @@
 package vemundrefnin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 public class App extends JavaPlugin {
     @Override
@@ -8,5 +11,21 @@ public class App extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("See you again, SpigotMC!");
+    }
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+        if (command.getName().equals("die")) {
+            if(sender instanceof Player){
+                Player player = (Player) sender;
+                player.setHealth(0);
+                player.sendMessage("die");
+            }
+            else {
+                System.out.println("You need to be a player to run this command");
+            }
+        }
+        return false;
     }
 }
