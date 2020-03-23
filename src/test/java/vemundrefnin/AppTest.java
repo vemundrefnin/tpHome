26839d55b55fe003d7728d6cb4f73f8d19127e5d
Vemund Refnin <vemund.refnin@gmail.com>
2020-03-23 22:20:22 +0100

    First commit

---
+++ src/test/java/vemundrefnin/AppTest.java
@@ -0,0 +1,20 @@
+package vemundrefnin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
